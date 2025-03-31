360b886b2ae1f65ab73d8152d66f2adb78a38115
Hugo BAUX <hugo.baux@etu.inp-n7.fr>
2025-03-31 18:58:47 +0200

    little changes

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, "myapp/index.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "myapp/login.html", context)
